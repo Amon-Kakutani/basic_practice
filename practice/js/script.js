bdf7c545e56f4f45f20e57bc5969155bb9bb1217
KAKUTANI AMON <kyodansiamon@KAKUTANInoMacBook-Pro.local>
2020-10-01 14:28:09 +0900

    良いい

---
+++ practice/js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+
+    $(".btn").on("click",()=>{
+        $("#slide").slideToggle();
+    })
+
+    $("#slide").slick({
+        dots:true,
+        slideToshow:1,
+        slidesToScroll:1,
+        autoplay:true,
+        autoplaySpeed:3000,});
+
+});
